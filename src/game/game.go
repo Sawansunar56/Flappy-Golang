4be9ffbe82b9590a2c7c2f38f3408d47bd699d6c
Sawansunar56 <sawansunar24072002@gmail.com>
2023-09-30 10:05:22 +0530

    moving boxes done
    Reimplemented the render method to now use an array for the display

--- src/game/game.go
+++ src/game/game.go
@@ -22,33 +22,33 @@ func randomNumberGenerator(min, max int, screen tcell.Screen) {
 	}
 }
 
+// Aligns a new top box in the display
 func renderTopBox(screen tcell.Screen, x, y int, display [wide][high]rune) [wide][high]rune {
-  bottomLeft := '└'
+	bottomLeft := '└'
 	bottomRight := '┘'
 	horizontalLine := '─'
 	verticalLine := '│'
-  display[10][10] = 'x'
+
+	display[10][10] = 'x'
 	for i := 1; i <= y; i++ {
 		display[x][y-i] = verticalLine
-		// screen.SetContent(x, y-i, verticalLine, nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
 	}
 
 	display[x][y] = bottomLeft
-	// screen.SetContent(x, y, bottomLeft, nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
 	for i := 1; i <= 10; i++ {
 		display[x+i][y] = horizontalLine
-		// screen.SetContent(x+i, y, horizontalLine, nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
 	}
+
 	x = x + 10
 	display[x][y] = bottomRight
-	// screen.SetContent(x, y, bottomRight, nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
 	for i := 1; i <= y; i++ {
 		display[x][y-i] = verticalLine
-		// screen.SetContent(x, y-i, verticalLine, nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
 	}
-  return display
+
+	return display
 }
 
+// Aligns a new bottom box into the display
 func renderBottomBox(screen tcell.Screen, x, y int, display [wide][high]rune) [wide][high]rune {
 	_, height := screen.Size()
 	topLeft := '┌'
@@ -58,30 +58,44 @@ func renderBottomBox(screen tcell.Screen, x, y int, display [wide][high]rune) [w
 
 	for i := height - 1; i > y+10; i-- {
 		display[x][i] = verticalLine
-		// screen.SetContent(x, i, verticalLine, nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
 	}
+
 	y = y + 10
 	display[x][y] = topLeft
-	// screen.SetContent(x, y, topLeft, nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
 	for i := 1; i <= 10; i++ {
 		display[x+i][y] = horizontalLine
-		// screen.SetContent(x+i, y, horizontalLine, nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
 	}
+
 	x = x + 10
 	display[x][y] = topRight
-	// screen.SetContent(x, y, topRight, nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
 	for i := height - 1; i > y; i-- {
 		display[x][i] = verticalLine
-		// screen.SetContent(x, i, verticalLine, nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
 	}
-  return display
+
+	return display
 }
 
 func platform(screen tcell.Screen, x, y int) {
 }
 
-func Game(screen tcell.Screen) {
+func renderNewBox(screen tcell.Screen, display [wide][high]rune) [wide][high]rune {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	randomNumber := r.Intn(30-3+1) + 3
+	display = renderTopBox(screen, 130, randomNumber, display)
+	display = renderBottomBox(screen, 130, randomNumber, display)
+	return display
+}
+
+func renderBack(display [wide][high]rune, width, height int) [wide][high]rune {
+	for i := 0; i < width-1; i++ {
+		for j := 0; j < height; j++ {
+			display[i][j] = display[i+1][j]
+		}
+	}
+	return display
+}
+
+func Game(screen tcell.Screen) {
 	width, height := screen.Size()
 	var display [wide][high]rune
 
@@ -92,19 +106,7 @@ func Game(screen tcell.Screen) {
 	for i, char := range text {
 		screen.SetContent(x+i, y, char, nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
 	}
-	// screen.SetContent(x, 10, 'H', nil, tcell.StyleDefault.Foreground(tcell.ColorHotPink))
-	// platform(screen, 5, 10)
-	// i, j := 5, 10
 
-	for i := 1; i <= 3; i++ {
-		randomNumber := r.Intn(30-3+1) + 3
-		display = renderTopBox(screen, 30*i, randomNumber, display)
-		display = renderBottomBox(screen, 30*i, randomNumber, display)
-	}
-
-  // display[154][0] = 'h'
-	// display = renderTopBox(screen, i, j, display)
-	// display = renderBottomBox(screen, i, j, display)
 	for i := 0; i < width; i++ {
 		for j := 0; j < height; j++ {
 			screen.SetContent(i, j, display[i][j], nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
@@ -123,6 +125,22 @@ func Game(screen tcell.Screen) {
 				return // Exit the application on Ctrl+C
 			} else if keyEvent.Rune() == 'i' {
 				screen.Clear()
+				display = renderBack(display, width, height)
+				for i := 0; i < width; i++ {
+					for j := 0; j < height; j++ {
+						screen.SetContent(i, j, display[i][j], nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
+					}
+				}
+				screen.Show()
+			} else if keyEvent.Rune() == 'x' {
+				screen.Clear()
+				display = renderNewBox(screen, display)
+
+				for i := 0; i < width; i++ {
+					for j := 0; j < height; j++ {
+						screen.SetContent(i, j, display[i][j], nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
+					}
+				}
 				screen.Show()
 			}
 		}
